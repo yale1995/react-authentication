53a83413e32fce9c4b4b8c22d81ab2e83f0d5618
Yale Araujo <yale850@gmail.com>
2023-01-20 11:08:20 -0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+'use client'
+import { FormEvent, useState, useContext } from 'react'
+import {AuthContext} from '../contexts/AuthContext'
+import styles from './home.module.css'
+
 export default function Home() {
-  return (<div>
-    <h1>OBRIGADO JESUS CRISTO!</h1>
-  </div>)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const {signIn, isAuthenticated} = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    const data = {
+      email,
+      password,
+    }
+    await signIn(data)
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <form onSubmit={handleSubmit} className={styles.container}>
+        <input type='email' value={email} onChange={(event) => setEmail(event.target.value)} />
+        <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} />
+        <button type='submit'>Entrar</button>
+      </form>
+    </div>
+  )
 }
